1643b6043273df86d2dbda3e64113a75515aedf8
ygqygq2 <29ygq@sina.com>
2022-03-19 14:07:40 +0800

    fix: 修复启动异常时出现的问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #set -e
+if [ "$debug" = "true" ]; then
+    set -x
+fi
 
 GROUP_NAME=${GROUP_NAME:-group1}
 if [ -n "$GET_TRACKER_SERVER" ]; then
     export TRACKER_SERVER=$(eval $GET_TRACKER_SERVER)
 fi
 
-function fdfs_set () {
+function fdfs_set() {
     if [ "$1" = "monitor" ] ; then
         if [ -n "$TRACKER_SERVER" ] ; then  
-          sed -i "s|tracker_server=.*$|tracker_server=${TRACKER_SERVER}|g" /etc/fdfs/client.conf
+          sed -i "s|tracker_server[ ]*=[ ]*.*$|tracker_server = ${TRACKER_SERVER}|g" /etc/fdfs/client.conf
         fi
         fdfs_monitor /etc/fdfs/client.conf
         exit 0
@@ -20,29 +23,37 @@ function fdfs_set () {
     fi
     
     if [ -n "$PORT" ] ; then  
-        sed -i "s|^port=.*$|port=${PORT}|g" /etc/fdfs/"$FASTDFS_MODE".conf
+        sed -i "s|^port[ ]*=[ ]*.*$|port = ${PORT}|g" /etc/fdfs/"$FASTDFS_MODE".conf
     fi
     
     if [ -n "$TRACKER_SERVER" ] ; then  
-        sed -i "s|tracker_server=.*$|tracker_server=${TRACKER_SERVER}|g" /etc/fdfs/storage.conf
-        sed -i "s|tracker_server=.*$|tracker_server=${TRACKER_SERVER}|g" /etc/fdfs/client.conf
-        sed -i "s|tracker_server=.*$|tracker_server=${TRACKER_SERVER}|g" /etc/fdfs/mod_fastdfs.conf
+        sed -i "s|tracker_server[ ]*=[ ]*.*$|tracker_server = ${TRACKER_SERVER}|g" /etc/fdfs/storage.conf
+        sed -i "s|tracker_server[ ]*=[ ]*.*$|tracker_server = ${TRACKER_SERVER}|g" /etc/fdfs/client.conf
+        sed -i "s|tracker_server[ ]*=[ ]*.*$|tracker_server = ${TRACKER_SERVER}|g" /etc/fdfs/mod_fastdfs.conf
     fi
     
-    sed -i "s|group_name=.*$|group_name=${GROUP_NAME}|g" /etc/fdfs/storage.conf
+    sed -i "s|group_name[ ]*=[ ]*.*$|group_name = ${GROUP_NAME}|g" /etc/fdfs/storage.conf
+    sed -i "s|group_name[ ]*=[ ]*.*$|group_name = ${GROUP_NAME}|g" /etc/fdfs/mod_fastdfs.conf
+}
     
+function fdfs_start() {
     FASTDFS_LOG_FILE="${FASTDFS_BASE_PATH}/logs/${FASTDFS_MODE}d.log"
     PID_NUMBER="${FASTDFS_BASE_PATH}/data/fdfs_${FASTDFS_MODE}d.pid"
     
     echo "try to start the $FASTDFS_MODE node..."
+    fdfs_${FASTDFS_MODE}d /etc/fdfs/${FASTDFS_MODE}.conf stop
     if [ -f "$FASTDFS_LOG_FILE" ]; then 
-        rm "$FASTDFS_LOG_FILE"
+        rm -f "$FASTDFS_LOG_FILE"
     fi
+    if [ -f "$PID_NUMBER" ]; then
+        rm -f "$PID_NUMBER"
+    fi
+
     # start the fastdfs node.	
     fdfs_${FASTDFS_MODE}d /etc/fdfs/${FASTDFS_MODE}.conf start
 }
 
-function nginx_set () {
+function nginx_set() {
     # start nginx.
     if [ "${FASTDFS_MODE}" = "storage" ]; then
         cp -f /nginx_conf/conf.d/${FASTDFS_MODE}.conf /usr/local/nginx/conf/conf.d/
@@ -64,8 +75,7 @@ function health_check() {
         error_log=$(egrep "OFFLINE" "$check_log")
         if [ ! -z "$error_log" ]; then
             cat /dev/null > "$FASTDFS_LOG_FILE"
-            fdfs_${FASTDFS_MODE}d /etc/fdfs/${FASTDFS_MODE}.conf stop
-            fdfs_${FASTDFS_MODE}d /etc/fdfs/${FASTDFS_MODE}.conf start
+            fdfs_start
         fi
         sleep 10
     done
@@ -73,16 +83,23 @@ function health_check() {
 
 fdfs_set $*
 nginx_set $*
+fdfs_start
 health_check &
 
-# wait for pid file(important!),the max start time is 5 seconds,if the pid number does not appear in 5 seconds,start failed.
-TIMES=5
+# wait for pid file(important!),the max start time is 30 seconds,if the pid number does not appear in 30 seconds,start failed.
+TIMES=30
 while [ ! -f "$PID_NUMBER" -a $TIMES -gt 0 ]
 do
     sleep 1s
     TIMES=`expr $TIMES - 1`
 done
 
-tail -f "$FASTDFS_LOG_FILE"
-
-exec "$@"
+case $1 in
+    monitor|storage|tracker)
+    # sleep infinity
+    [ -f "$FASTDFS_LOG_FILE" ] && tail -f "$FASTDFS_LOG_FILE"
+    ;;
+    *)
+    exec "$@"
+    ;;
+esac
